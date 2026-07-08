f2b34274b4a3bc53498df2bfb69a858346630f1a
DAT DINH TIEN <datb2408781@gmail.com>
2026-07-08 19:32:39 +0700

    first header and footer

--- assets/script.js
+++ assets/script.js
@@ -2,163 +2,37 @@ import {
   fetchApi
 } from "./fetchApi.js";
 
-function saveUserInfo() {
-  let displayDate = '';
-  const dateOfBirth = localStorage.getItem('dateOfBirth');
-  if (dateOfBirth) {
-    const date = new Date(dateOfBirth);
-    date.getDate();
-    displayDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
-  }
-  document.getElementById('displayName').textContent = localStorage.getItem("fullName");
-  document.getElementById('displayEmail').textContent = localStorage.getItem("Email") || 'Không có';
-  document.getElementById('displayPhone').textContent = localStorage.getItem("phoneNumber");
-  document.getElementById('displayBirthday').textContent = displayDate;
-  document.getElementById('displayGender').textContent = localStorage.getItem("sex");
-  document.getElementById('empty-state').style.display = 'none';
-  document.getElementById('infor-display').classList.add('show');
+function getRemainingProducts() {
+  return document.querySelectorAll(".column-products .grid-products");
 }
 
-const form = document.querySelector("#personalForm");
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  const fullName = document.getElementById('fullName');
-  const email = document.getElementById('Email');
-  const phoneNumber = document.getElementById('phoneNumber');
-  const dateOfBirth = document.getElementById('dateOfBirth');
-  const sex = document.getElementById('sex');
-
-  let displayDate = '';
-  if (dateOfBirth) {
-    const date = new Date(dateOfBirth);
-    displayDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
-  }
-
-  localStorage.setItem(fullName.name, fullName.value);
-  localStorage.setItem(email.name, email.value);
-  localStorage.setItem(phoneNumber.name, phoneNumber.value);
-  localStorage.setItem(dateOfBirth.name, dateOfBirth.value);
-  localStorage.setItem(sex.name, sex.value);
-
-  saveUserInfo();
-
-  const col_right_UserInfo = document.querySelector(".section-4 .col-right");
-  if (col_right_UserInfo.classList.contains("hidden")) {
-    col_right_UserInfo.classList.remove("hidden");
-    col_right_UserInfo.classList.add("show");
-  }
-})
-
-const btn_UserInfo = document.querySelector(".section-4 .col-right button");
-btn_UserInfo.addEventListener('click', () => {
-  const section_5 = document.querySelector(".section-5");
-  if (section_5.classList.contains("hidden")) {
-    section_5.classList.remove("hidden");
-    section_5.classList.add("show");
-  }
-})
-
-// document.querySelectorAll(".imgs img").forEach((img, index) => {
-//   let startY = img.getBoundingClientRect().top + window.scrollY;
-//   const speed = 0.1;
-
-//   window.addEventListener("scroll", function () {
-//     const scrolled = window.scrollY;
-//     img.style.transform = `translateY(${(scrolled - startY) * speed}px)`;
-//   });
-// });  
-
-function displayOrderInfo() {
-  const fullName = localStorage.getItem('fullName');
-  console.log(fullName);
-  const email = localStorage.getItem('Email');
-  console.log(email);
-  const phoneNumber = localStorage.getItem('phoneNumber');
-  console.log(phoneNumber);
-  const dateOfBirth = localStorage.getItem('dateOfBirth');
-  console.log(dateOfBirth);
-  const gender = localStorage.getItem('sex');
-  console.log(gender);
-  const order_Date = localStorage.getItem('order_Date');
-  console.log(order_Date);
-
-  document.querySelector(".section-6 .buyer-card__body .buyer-name span").innerHTML = fullName;
-  document.querySelector(".section-6 .buyer-card__body .buyer-email span").innerHTML = email;
-  document.querySelector(".section-6 .buyer-card__body .buyer-phoneNumber span").innerHTML = phoneNumber;
-  document.querySelector(".section-6 .buyer-card__body .buyer-dateOfBirth span").innerHTML = dateOfBirth;
-  document.querySelector(".section-6 .buyer-card__body .buyer-gender span").innerHTML = gender;
-  document.querySelector(".section-6 .buyer-card__body .buyer-orderDate span").innerHTML = order_Date;
-  
+function updateTotalPrice(gridProduct, quantity) {
+  const priceElement = gridProduct.querySelector('.col-price');
+  const totalElement = gridProduct.querySelector('.col-total');
+  const priceNumber = parseInt(priceElement.textContent.replace(/,/g, ''));
+  const total = priceNumber * quantity;
+  totalElement.textContent = total.toLocaleString('en-US') + ' VNĐ';
 }
 
-
-function selectBank() {
-  document.querySelectorAll(".card-item").forEach(card => {
-    card.addEventListener('click', function () {
-      document.querySelectorAll('.bank-value').forEach(el => {
-        el.classList.remove("active");
-        el.classList.add("hidden");
-      });
-
-      const card_left = document.querySelector(".grid-card-left");
-      card_left.classList.add("active");
-      const card_right = document.querySelector(`#bankData-${this.id}`);
-      card_right.classList.add("active")
-      const bankName = this.getAttribute('name');
-      const btn_credit = document.querySelector(".section-5 .payment-body .button-1");
-      btn_credit.onclick = () => {
-        if (confirm("Chấp nhận chuyển khoản")) {
-          localStorage.setItem('bankName', bankName);
-          const time_now = new Date();
-          const order_Date = time_now.toUTCString();
-          localStorage.setItem("order_Date", order_Date);
-
-
-          const section_6 = document.querySelector(".section-6");
-          if (section_6.classList.contains("hidden")) {
-            section_6.classList.remove("hidden");
-            section_6.classList.add("show");
-          }
-        }
-      }
-    });
+function calculate() {
+  let total = 0;
+  const grid_products = document.querySelectorAll(".section-3 .user-card-body .column-products .grid-products");
+  grid_products.forEach(item => {
+    const checkBox = item.querySelector(".col-action input");
+    const totalElement = item.querySelector(".col-total");
+    if (checkBox.checked) {
+      const priceNumber = parseInt(totalElement.textContent.replace(/,/g, ''));
+      total += priceNumber;
+    }
   });
-  displayOrderInfo();
+  const format_total = total.toLocaleString('en-US') + " VND";
+  const column_total_price = document.querySelector(".section-3 .user-card-body .column-total-price div span");
+  const amount_currency = document.querySelector(".section-5 .payment .payment-body .payment-amount .amount-value");
+  amount_currency.innerHTML = format_total;
+  column_total_price.innerHTML = format_total;
 }
 
-
-fetchApi("http://localhost:3000/banks")
-  .then(data => {
-    let htmls = "";
-    let value = "";
-    const name = document.querySelector("#fullName");
-    const phoneNumber = document.querySelector("#phoneNumber");
-    data.forEach(item => {
-      htmls += `
-      <li class="card-item" name="${item.name}" id="${item.id}">
-        <img src="${item.logo}" alt="${item.shortName}">
-        <div class="card-title">${item.name}</div>
-        <div class="card-circle"></div>
-      </li>
-      `
-      value += `
-      <div class="bank-value hidden" id="bankData-${item.id}">
-        <div class="bank-name-value">${item.shortName}</div>
-        <div class="account-holder-value">Bright English</div>
-        <div class="account-number-value">${item.accountNumber}</div>
-        <div class="content-value">....</div>
-      </div>`
-    });
-    const bank = document.querySelector("#bank")
-    bank.innerHTML = htmls;
-    const bankValue = document.querySelector(".grid-card-right")
-    bankValue.innerHTML = value;
-    selectBank();
-  });
-
-
-
-
+//section-3
 function quantityButtons() {
   const btn_minus = document.querySelectorAll(".btn-minus");
   btn_minus.forEach(function (btn) {
@@ -193,14 +67,6 @@ function quantityButtons() {
   });
 }
 
-function updateTotalPrice(gridProduct, quantity) {
-  const priceElement = gridProduct.querySelector('.col-price');
-  const totalElement = gridProduct.querySelector('.col-total');
-  const priceNumber = parseInt(priceElement.textContent.replace(/,/g, ''));
-  const total = priceNumber * quantity;
-  totalElement.textContent = total.toLocaleString('en-US') + ' VNĐ';
-}
-
 
 function checkBoxEvent() {
   const allCheckBox = document.querySelector(".column-feature #selectAll");
@@ -229,19 +95,13 @@ function checkBoxEvent() {
   });
 }
 
-function getRemainingProducts() {
-  return document.querySelectorAll(".column-products .grid-products");
-}
-
 function deleteEvent() {
   const deleteAllButton = document.querySelector(".column-feature .col-del i");
-  console.log(deleteAllButton);
   const deleteButtons = document.querySelectorAll(".column-products .col-del i");
   deleteAllButton.addEventListener('click', function () {
     if (confirm("xoá tất cả sản phẩm trong giỏ hàng")) {
       deleteButtons.forEach(btn => {
         const grid_product = btn.closest(".grid-products");
-        console.log(grid_product);
         grid_product.remove();
         const productId = grid_product.id;
         localStorage.removeItem(productId);
@@ -250,14 +110,12 @@ function deleteEvent() {
   })
 
   let remainingProducts = getRemainingProducts();
-  console.log(deleteButtons);
   deleteButtons.forEach(item => {
     item.addEventListener('click', function () {
       const grid_product = this.closest(".grid-products");
       grid_product.remove();
       const productId = grid_product.id;
       localStorage.removeItem(productId);
-      console.log(productId);
       let col_product_feature = document.querySelector(".column-feature .col-product");
       col_product_feature.innerHTML = `Tất cả (${remainingProducts.length} sản phẩm)`;
       calculate();
@@ -277,6 +135,28 @@ function deleteEvent() {
   }
 }
 
+function saveSelectedProducts() {
+  const selectedProducts = [];
+  const checkBoxes = document.querySelectorAll(".column-products .grid-products .col-action input:checked");
+
+  checkBoxes.forEach(item => {
+    const gridProduct = item.closest(".grid-products");
+    if (gridProduct) {
+      const product = {
+        id: gridProduct.id,
+        name: gridProduct.querySelector('.col-product span').textContent,
+        price: gridProduct.querySelector('.col-price').textContent,
+        quantity: gridProduct.querySelector('.quantity').textContent,
+        total: gridProduct.querySelector('.col-total').textContent,
+        thumbnail: gridProduct.querySelector('.col-product img').src
+      }
+      selectedProducts.push(product);
+      console.log(selectedProducts);
+    }
+  });
+  return selectedProducts;
+}
+
 function btn_products() {
   const btn_submit = document.querySelector(".btn__user-card");
   btn_submit.addEventListener('click', () => {
@@ -288,6 +168,9 @@ function btn_products() {
       }
     });
     if (hasBoxChecked) {
+
+      saveSelectedProducts();
+
       const section_4 = document.querySelector(".section-4");
       if (section_4.classList.contains("hidden")) {
         section_4.classList.remove("hidden");
@@ -298,6 +181,102 @@ function btn_products() {
     }
   });
 }
+
+// end section-3
+
+// section-4
+function saveUserInfo() {
+  let displayDate = '';
+  const dateOfBirth = localStorage.getItem('dateOfBirth');
+  if (dateOfBirth) {
+    const date = new Date(dateOfBirth);
+    date.getDate();
+    displayDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+  }
+  document.getElementById('displayName').textContent = localStorage.getItem("fullName");
+  document.getElementById('displayEmail').textContent = localStorage.getItem("Email") || 'Không có';
+  document.getElementById('displayPhone').textContent = localStorage.getItem("phoneNumber");
+  document.getElementById('displayBirthday').textContent = displayDate;
+  document.getElementById('displayGender').textContent = localStorage.getItem("sex");
+  document.getElementById('empty-state').style.display = 'none';
+  document.getElementById('infor-display').classList.add('show');
+}
+// end section-4
+
+// section-5
+
+function selectBank() {
+  document.querySelectorAll(".card-item").forEach(card => {
+    card.addEventListener('click', function () {
+      document.querySelectorAll('.bank-value').forEach(el => {
+        el.classList.remove("active");
+        el.classList.add("hidden");
+      });
+
+      const card_left = document.querySelector(".grid-card-left");
+      card_left.classList.add("active");
+      const card_right = document.querySelector(`#bankData-${this.id}`);
+      card_right.classList.add("active")
+      const bankName = this.getAttribute('name');
+      const btn_credit = document.querySelector(".section-5 .payment-body .button-1");
+      btn_credit.onclick = () => {
+        if (confirm("Chấp nhận chuyển khoản")) {
+          localStorage.setItem('bankName', bankName);
+          const time_now = new Date();
+          const order_Date = time_now.toUTCString();
+          localStorage.setItem("order_Date", order_Date);
+          displayOrderInfo();
+          const section_6 = document.querySelector(".section-6");
+          if (section_6.classList.contains("hidden")) {
+            section_6.classList.remove("hidden");
+            section_6.classList.add("show");
+          }
+        }
+      }
+    });
+  });
+}
+// end section_5
+
+// section 6
+function displayOrderInfo() {
+  const fullName = localStorage.getItem('fullName');
+  const email = localStorage.getItem('Email');
+  const phoneNumber = localStorage.getItem('phoneNumber');
+  const dateOfBirth = localStorage.getItem('dateOfBirth');
+  const gender = localStorage.getItem('sex');
+  const order_Date = localStorage.getItem('order_Date');
+
+  document.querySelector(".section-6 .buyer-card__body .buyer-name span").innerHTML = fullName;
+  document.querySelector(".section-6 .buyer-card__body .buyer-email span").innerHTML = email;
+  document.querySelector(".section-6 .buyer-card__body .buyer-phoneNumber span").innerHTML = phoneNumber;
+  document.querySelector(".section-6 .buyer-card__body .buyer-dateOfBirth span").innerHTML = dateOfBirth;
+  document.querySelector(".section-6 .buyer-card__body .buyer-gender span").innerHTML = gender;
+  document.querySelector(".section-6 .buyer-card__body .buyer-orderDate span").innerHTML = order_Date;
+
+  const courseList = document.querySelector(".course-list__body");
+  const selectedProducts = saveSelectedProducts();
+  console.log(selectedProducts);
+  const totalCouseList = document.querySelector(".course-list__count");
+  totalCouseList.innerHTML = `${selectedProducts.length} khoá học`;
+  let htmls = '';
+  selectedProducts.forEach(item => {
+    htmls += `
+    <div class="course-item">
+      <img src="${item.thumbnail}" alt="Course"
+          class="course-item__thumb">
+      <div class="course-item__name">${item.name}</div>
+      <div class="course-item__price">
+          <span class="course-item__price-value">${item.price}</span>
+          <span class="course-item__price-x">x${item.quantity}</span>
+      </div>
+    </div>
+    `
+  });
+  courseList.innerHTML = htmls;
+}
+// end section-6
+
 
 const allKeys = Object.keys(localStorage);
 const productKeys = allKeys.filter(key => key.startsWith('Product-'));
@@ -356,21 +335,89 @@ if (productKeys.length > 0) {
     });
 }
 
-function calculate() {
-  let total = 0;
-  const grid_products = document.querySelectorAll(".section-3 .user-card-body .column-products .grid-products");
-  grid_products.forEach(item => {
-    const checkBox = item.querySelector(".col-action input");
-    const totalElement = item.querySelector(".col-total");
-    if (checkBox.checked) {
-      const priceNumber = parseInt(totalElement.textContent.replace(/,/g, ''));
-      total += priceNumber;
-    }
+
+fetchApi("http://localhost:3000/banks")
+  .then(data => {
+    let htmls = "";
+    let value = "";
+    const name = document.querySelector("#fullName");
+    const phoneNumber = document.querySelector("#phoneNumber");
+    data.forEach(item => {
+      htmls += `
+      <li class="card-item" name="${item.name}" id="${item.id}">
+        <img src="${item.logo}" alt="${item.shortName}">
+        <div class="card-title">${item.name}</div>
+        <div class="card-circle"></div>
+      </li>
+      `
+      value += `
+      <div class="bank-value hidden" id="bankData-${item.id}">
+        <div class="bank-name-value">${item.shortName}</div>
+        <div class="account-holder-value">Bright English</div>
+        <div class="account-number-value">${item.accountNumber}</div>
+        <div class="content-value">....</div>
+      </div>`
+    });
+    const bank = document.querySelector("#bank")
+    bank.innerHTML = htmls;
+    const bankValue = document.querySelector(".grid-card-right")
+    bankValue.innerHTML = value;
+    selectBank();
   });
-  const format_total = total.toLocaleString('en-US') + " VND";
-  const column_total_price = document.querySelector(".section-3 .user-card-body .column-total-price div span");
-  const amount_currency = document.querySelector(".section-5 .payment .payment-body .payment-amount .amount-value");
-  amount_currency.innerHTML = format_total;
-  column_total_price.innerHTML = format_total;
-}
+
+
+
+const form = document.querySelector("#personalForm");
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const fullName = document.getElementById('fullName');
+  const email = document.getElementById('Email');
+  const phoneNumber = document.getElementById('phoneNumber');
+  const dateOfBirth = document.getElementById('dateOfBirth');
+  const sex = document.getElementById('sex');
+
+  let displayDate = '';
+  if (dateOfBirth) {
+    const date = new Date(dateOfBirth);
+    displayDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+  }
+
+  localStorage.setItem(fullName.name, fullName.value);
+  localStorage.setItem(email.name, email.value);
+  localStorage.setItem(phoneNumber.name, phoneNumber.value);
+  localStorage.setItem(dateOfBirth.name, dateOfBirth.value);
+  localStorage.setItem(sex.name, sex.value);
+  saveUserInfo();
+  const col_right_UserInfo = document.querySelector(".section-4 .col-right");
+  if (col_right_UserInfo.classList.contains("hidden")) {
+    col_right_UserInfo.classList.remove("hidden");
+    col_right_UserInfo.classList.add("show");
+  }
+})
+
+const btn_UserInfo = document.querySelector(".section-4 .col-right button");
+btn_UserInfo.addEventListener('click', () => {
+  const section_5 = document.querySelector(".section-5");
+  if (section_5.classList.contains("hidden")) {
+    section_5.classList.remove("hidden");
+    section_5.classList.add("show");
+  }
+})
+
+
+// document.querySelectorAll(".imgs img").forEach((img, index) => {
+//   let startY = img.getBoundingClientRect().top + window.scrollY;
+//   const speed = 0.1;
+
+//   window.addEventListener("scroll", function () {
+//     const scrolled = window.scrollY;
+//     img.style.transform = `translateY(${(scrolled - startY) * speed}px)`;
+//   });
+// });  
+
+
+
+
+
+
 
